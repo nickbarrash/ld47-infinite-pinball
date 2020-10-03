95aae5994111b10abfad2cf1eb5fbbb1b5aef6a5
Nick Barrash <nickbarrash@gmail.com>
2020-10-03 19:52:48 -0400

    trying something descructive save

---
+++ Infinite Pinball/Assets/Scripts/ClonableArena.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonableArena : MonoBehaviour
+{
+    public static bool instantiated = false;
+
+    public float offset = 150f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!instantiated) {
+            instantiated = true;
+            GameObject upperArena = Instantiate(gameObject);
+            GameObject lowerArena = Instantiate(gameObject);
+            upperArena.transform.position += new Vector3(0, offset, 0);
+            lowerArena.transform.position -= new Vector3(0, offset, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
