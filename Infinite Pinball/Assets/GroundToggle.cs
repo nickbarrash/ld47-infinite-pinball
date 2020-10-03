91989ae9533241aea14622ac35635efa016f581e
Nick Barrash <nickbarrash@gmail.com>
2020-10-03 00:07:20 -0400

    v0.5.0 light up gates

---
+++ Infinite Pinball/Assets/GroundToggle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundToggle : MonoBehaviour
+{
+    Material toggleMaterial;
+
+    bool isOn = false;
+
+    void Start() {
+        toggleMaterial = GetComponent<Renderer>().material;
+        UpdateColor();
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        Debug.Log("here");
+
+        if (collider.gameObject.layer != LayerMask.NameToLayer("Ball")) {
+            return;
+        }
+
+        Debug.Log("here2");
+
+        isOn = !isOn;
+
+        UpdateColor();
+    }
+
+    void UpdateColor() {
+        if (isOn) {
+            toggleMaterial.SetColor("_Color", new Color(1, 1, 0, 1));
+        } else {
+            toggleMaterial.SetColor("_Color", new Color(0.3f, 0.3f, 0, 1));
+        }
+    }
+}
---
+++ Infinite Pinball/Assets/SideBumper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideBumper : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Ball")) {
+            return;
+        }
+
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(collision.contacts[0].normal * 200f, ForceMode.Impulse);
+    }
+}
