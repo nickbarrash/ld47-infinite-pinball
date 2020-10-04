d8c98a132dbbf433e960fa4488e8eab25ec23ccf
Nick Barrash <nickbarrash@gmail.com>
2020-10-03 23:20:25 -0400

    v.1.3.4 fix spawn collision bug

--- Infinite Pinball/Assets/Scripts/ClonableArena.cs
+++ Infinite Pinball/Assets/Scripts/ClonableArena.cs
@@ -13,10 +13,8 @@ public class ClonableArena : MonoBehaviour
     {
         if (!instantiated) {
             instantiated = true;
-            GameObject upperArena = Instantiate(gameObject);
-            GameObject lowerArena = Instantiate(gameObject);
-            upperArena.transform.position += new Vector3(0, offset, 0);
-            lowerArena.transform.position -= new Vector3(0, offset, 0);
+            Instantiate(gameObject, transform.position + new Vector3(0, offset, 0), Quaternion.identity);
+            Instantiate(gameObject, transform.position - new Vector3(0, offset, 0), Quaternion.identity);
         }
     }
 
